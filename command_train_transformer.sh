c73115afb7c8bd4711abc9f7802a6e9902c9d849
Ehsan Hosseiniasl <ehsan@ip-10-20-8-215.us-west-2.compute.internal>
2018-09-29 21:59:56 +0000

    update cmd

--- command_train_transformer.sh
+++ command_train_transformer.sh
@@ -3,14 +3,18 @@ GPU=5
 CHECKPOINT=/export/home/ehsan/ParlAI/checkpoints
 LAYER=2
 HEAD=2
-DK=256
-DV=256
+DK=64
+DV=64
 D_MODEL=256
-D_INNER=1024
+D_INNER=256
 D_WORD=256
-EXPERIMENT=convai2_transformer_[l=${LAYER},h=${HEAD},dw=${D_WORD},dm=${D_MODEL},di=${D_INNER},dk=${DK},dv=${DV}]
+SRC_TGT=False
+TGT_PRJ=False
+SMOOTH=True
+
+EXPERIMENT=convai2_transformer_[l=${LAYER},h=${HEAD},dw=${D_WORD},dm=${D_MODEL},di=${D_INNER},dk=${DK},dv=${DV},src_tgt_share=${SRC_TGT},tgt_prj=${TGT_PRJ},smooth=${SMOOTH}]
 MODEL_FILE=$CHECKPOINT/$EXPERIMENT
 TAG=model,numlayers,n_head,d_word_vec,d_model,d_inner,d_k,d_v
 
-CUDA_VISIBLE_DEVICES=$GPU python projects/convai2/baselines/transformer/train.py --model_file $MODEL_FILE --tensorboard_tag $TAG --numlayers $LAYER --n_head $HEAD --d_word_vec $D_WORD --d_model $D_MODEL --d_inner $D_INNER --d_k $DK --d_v $DV
+CUDA_VISIBLE_DEVICES=$GPU python projects/convai2/baselines/transformer/train.py --model_file $MODEL_FILE --tensorboard_tag $TAG --numlayers $LAYER --n_head $HEAD --d_word_vec $D_WORD --d_model $D_MODEL --d_inner $D_INNER --d_k $DK --d_v $DV --src_tgt_weight_share $SRC_TGT --tgt_prj_weight_share $TGT_PRJ --smoothing $SMOOTH
 
