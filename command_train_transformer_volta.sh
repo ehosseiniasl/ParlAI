9271259eef0e930f2a7060c0f6fce5e22d8b437e
Ehsan Hosseiniasl <ehsan@ip-10-20-9-112.us-west-2.compute.internal>
2018-11-02 21:34:04 +0000

    add volta command

---
+++ command_train_transformer_volta.sh
@@ -0,0 +1,22 @@
+
+GPU=1
+CHECKPOINT=/export/home/ehsan/ParlAI/checkpoints
+LAYER=2
+HEAD=4
+DK=64
+DV=64
+D_MODEL=256
+D_INNER=2048
+D_WORD=256
+SRC_TGT=False
+TGT_PRJ=False
+SMOOTH=False
+MAX_LEN=600
+EPOCHS=20000
+
+#EXPERIMENT=convai2_transformer_volta_[l=${LAYER},h=${HEAD},dw=${D_WORD},dm=${D_MODEL},di=${D_INNER},dk=${DK},dv=${DV},src_tgt_share=${SRC_TGT},tgt_prj=${TGT_PRJ},smooth=${SMOOTH}]
+EXPERIMENT=convai2_transformer_volta_[l=${LAYER},h=${HEAD}]
+MODEL_FILE=$CHECKPOINT/$EXPERIMENT
+TAG=model,numlayers,n_head
+
+CUDA_VISIBLE_DEVICES=$GPU python projects/convai2/baselines/transformer/train.py --numworkers 4 -eps $EPOCHS --model_file $MODEL_FILE --tensorboard_tag $TAG --numlayers $LAYER --n_head $HEAD --d_word_vec $D_WORD --d_model $D_MODEL --d_inner $D_INNER --d_k $DK --d_v $DV --max_token_seq_len $MAX_LEN --src_tgt_weight_share $SRC_TGT --tgt_prj_weight_share $TGT_PRJ --label_smoothing $SMOOTH --embedding_type glove 
